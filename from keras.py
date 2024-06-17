23a13753c329c8c413603ba187c7c3e318b13e1c
NiawVnch <166097702+NiawVnch@users.noreply.github.com>
2024-06-17 08:11:49 +0700

    Update from keras.py

--- from keras.py
+++ from keras.py
@@ -19,13 +19,13 @@ while True:
     ret, image = camera.read()
 
     # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (112, 112), interpolation=cv2.INTER_AREA)
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
     # Show the image in a window
     cv2.imshow("Webcam Image", image)
 
     # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 112, 112, 3)
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
     # Normalize the image array
     image = (image / 127.5) - 1
